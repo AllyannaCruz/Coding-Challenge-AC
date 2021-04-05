f7ca1b74fb9449aeedc88b91ca44dbd59bfcfb0e
AllyannaCruz <55791523+AllyannaCruz@users.noreply.github.com>
2021-04-05 01:37:46 +0100

    json print

---
+++ allyannac/src/main/java/acruz/JSONProg.java
@@ -0,0 +1,16 @@
+package acruz;
+
+import org.json.JSONObject;
+
+public class JSONProg {
+    public static void main(String[] args) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("a", 1);
+        jsonObject.put("b",Boolean.valueOf(true));
+        JSONObject c = new JSONObject();
+        c.put("d",3);
+        c.put("e", "test");
+        //c.put("c",c);
+        System.out.println(jsonObject);
+    }
+}
