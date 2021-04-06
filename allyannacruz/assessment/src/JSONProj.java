7ab2782ccbd68feb00f036bd116004d0bbaa2acb
AllyannaCruz <55791523+AllyannaCruz@users.noreply.github.com>
2021-04-06 20:45:37 +0100

    stuff

--- allyannacruz/assessment/src/JSONProj.java
+++ allyannacruz/assessment/src/JSONProj.java
@@ -1,6 +1,10 @@
 /*
     Author: Allyanna Cruz
     Program Description: A program which takes in a JSON object as input and out puts a flattened version of the input.
+    The libraries used are specified in the README file. The user is prompted to choose between method 1 or 2.
+    I added two methods because method 2, JSONParse, parses the file specified into a JSON object, whereas method 1, 
+    JSONInput allows the user to input the key value pairs in, based on the test1.json file
+
 */
 
 package allyannacruz.assessment.src;
@@ -10,15 +14,8 @@ import org.json.JSONObject;
 import org.apache.commons.io.IOUtils;
 
 import java.util.Scanner;
-import java.util.*;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 
 public class JSONProj {
     public static void main(String[] args) throws Exception{
@@ -27,25 +24,7 @@ public class JSONProj {
         System.out.println("\n\nPress 1 to input JSON object \n or \nPress 2 to parse test1.txt");
         char inOrp = sc.next().charAt(0);
 
-        // if(inOrp == '1') {
-        //     JSONProj.JSONInput(); // Method which takes in user input to output a flattened JSON object
-        // }
-        // else if(inOrp == '2') {
-        //     JSONProj.JSONParse(); // Method which parses a .txt file in a JSON format and outputs the flattened JSON object
-        // }
-        // else {
-        //     System.out.println("Please input only either 1 or 2\n");
-        //     inOrp = sc.next().charAt(0);
-
-        //     if(inOrp == '1') {
-        //         JSONProj.JSONInput(); // Method which takes in user input to output a flattened JSON object
-        //     }
-            
-        //     if(inOrp == '2') {
-        //         JSONProj.JSONParse(); // Method which parses a .txt file in a JSON format and outputs the flattened JSON object
-        //     }
-        // } // end if else
-
+        // Switch statement to allow user to choose which method to use
         switch(inOrp) {
             case '1':
                 JSONProj.JSONInput(); // Method which takes in user input to output a flattened JSON object
@@ -61,6 +40,8 @@ public class JSONProj {
         sc.close();
     } // end main()
 
+    // This method takes in user input, based on the test1.txt or test1.json file.
+    // The ths json object is then printed and saved into whatever directory is specified in the path.
     public static void JSONInput() throws Exception {
         Scanner jo = new Scanner(System.in);
         JSONObject jsonObject = new JSONObject();
@@ -69,17 +50,11 @@ public class JSONProj {
         //jsonObject.put("a", 1);
         //jsonObject.put("b",Boolean.valueOf(true));
 
-        JSONObject c = new JSONObject();
-        //jsonObject.put("c",c );
-        //c.put("d",3);
-        //c.put("e", "test");
-        /*c.put("c",c);*/
-        //System.out.println(jsonObject);
-        /*System.out.println(c);*/
         System.out.println("Key 1:" );
         String key1 = jo.nextLine();
         System.out.println("Value 1:" );
         String value1 = jo.nextLine();
+        //int value1 = jo.nextInt();
         jsonObject.put(key1,value1);
         System.out.println("Key 2: ");
         String key2 = jo.nextLine();
@@ -90,52 +65,44 @@ public class JSONProj {
         String key3 = jo.nextLine();
         System.out.println("Value 3: ");
         String value3 = jo.nextLine();
-        //jsonObject.put(key3,value3);
-        if(value3 == "none") {
-            System.out.println("Enter key and value"); 
-            
-        } // end if
+
         
         System.out.println("Key 4: ");
         String key4 = jo.nextLine();
         System.out.println("Value 4: ");
         String value4 = jo.nextLine();
         jsonObject.put(key3 + "." + key4,value4);
-        // if(key3 == "d") jsonObject.put(key3 + "." + value3)
         System.out.println("Key 5: ");
         String key5 = jo.nextLine();
         System.out.println("Value 5: ");
         String value5 = jo.nextLine();
         jsonObject.put(key3 + "." + key5,value5);
 
-        //(key(i) || value(i) == "STOP")
-        //break; - stops the loop.
-
-        // String letterD = jsonObject.getJSONObject("c").get("d").toString();
-        // String letterE = jsonObject.getJSONObject("c").get("e").toString();
-        // String letterC = jsonObject.getJSONObject("c").toString();
         System.out.println(jsonObject);
+        
         jo.close();
         
-        // System.out.println(jsonObject);
-        // System.out.println(letterD);
-        // System.out.println(letterE);
-        // System.out.println(letterC);
+        String jsonFStr = jsonObject.toString();
 
-        // c.remove("d");
-        // c.remove("e");
-        // jsonObject.remove("c");
-        // c.put("c.d", 3);
-        // c.put("c.e", "test");
-        // jsonObject.put("c" + "." + "d", 3);
-        // jsonObject.put("c" + "." + "e", "test");
-        // System.out.println(jsonObject);
-
-        //object c
-        //JSONProj jp = new JSONProj();
+        // Please replace the path to your own path.
+        // The .json file will be saved as output1.json
+        try {
+            FileWriter fw = new FileWriter("C:/Users/acruz/OneDrive - Technological University Dublin/Documents/School/Year 3/Year 3 Semester 2/MongoDB Coding Challenge/allyannacruz/assessment/src/output1.json");
+            fw.write(jsonFStr);
+            fw.close();
+            System.out.println("\nFile saved in your folder");
+        } catch (Exception e) {
+            System.out.println("Cannot write file" + e.toString());
+        } 
     } // end JSONInput()
 
 
+    // This method parses the test1.txt which contains the json test format.
+    // The method converts the .txt file into a String and into a JSON object.
+    // The method removes the key "c" and replaces it with the keys "c.d" and "c.e"
+    // The jsonF JSON object is then converted into a string so that it can be wriiten into a new .json file
+    // Please change the path according to your own path
+    // The .json file is saved as output2.json
     public static void JSONParse() throws Exception {
         InputStream is = JSONProj.class.getResourceAsStream("test1.txt");
         String testTxt  = IOUtils.toString(is, "UTF-8");
@@ -146,22 +113,20 @@ public class JSONProj {
 
         String letterD = jsonF.getJSONObject("c").get("d").toString();
         String letterE = jsonF.getJSONObject("c").get("e").toString();
-        String letterC = jsonF.getJSONObject("c").toString();
         jsonF.remove("c");
         jsonF.put("c" + "." + "d", letterD);
         jsonF.put("c" + "." + "e", letterE);
         System.out.println(jsonF);
         String jsonFStr = jsonF.toString();
 
-        //PrintWriter pw = null;
-
+         // Please replace the path to your own path.
+        // The .json file will be saved as output2.json
         try {
-            //pw = new PrintWriter(new FileWriter("/output.json"));
-            FileWriter fw = new FileWriter("C:/Users/acruz/OneDrive - Technological University Dublin/Documents/School/Year 3/Year 3 Semester 2/MongoDB Coding Challenge/allyannacruz/assessment/src/output.json");
+            FileWriter fw = new FileWriter("C:/Users/acruz/OneDrive - Technological University Dublin/Documents/School/Year 3/Year 3 Semester 2/MongoDB Coding Challenge/allyannacruz/assessment/src/output2.json");
             fw.write(jsonFStr);
             fw.close();
+            System.out.println("\nFile saved in your folder");
         } catch (Exception e) {
-            //TODO: handle exception
             System.out.println("Cannot write file" + e.toString());
         } 
         
